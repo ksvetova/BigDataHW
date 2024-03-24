f84e36add5d4fccd6582bc481bfc7bf0efe112b7
ksvetova <58701924+ksvetova@users.noreply.github.com>
2024-03-24 13:34:01 +0100

    implemented algorithm for exact method

---
+++ HW1/ExactOutliers.py
@@ -0,0 +1,64 @@
+import math
+
+# function to compute distance between 2 points
+def distance(a, b):
+    # a = [x1, y1]
+    # b = [x2, y2]
+    d = math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
+    return d 
+
+
+# function to implement the exact algorithm
+def ExactOutliers(points, D, M, K):
+    outliers = []
+
+    for point in points:
+
+        set_points = 0 
+        for i in points:
+            if distance(i, point) <= D:
+                set_points += 1
+
+        if set_points <= M:
+            outliers.append((point, set_points))
+
+    print('Number of ouliers = ', len(outliers))
+
+    ouliers_sort = sorted(outliers, key=lambda a: a[1])
+
+    k = 0
+
+    if K > len(outliers):
+        K = len(outliers)
+    
+    for i in range(k, K):
+        print("first K outliers: ")
+        print(ouliers_sort[i][0])
+
+
+
+# function to read points from the file 
+def read_coordinates(file_path):
+
+    points = []
+    file = open(file_path, "r")
+    for line in file:
+        coord = line.strip().split(',')
+        points.append([float(i) for i in coord])
+    
+    return points
+
+
+
+if __name__ == "__main__":
+
+    file_path = 'test.txt'
+    points = read_coordinates(file_path)
+
+    D = float(input())
+    M = int(input())
+    K = int(input())
+
+    ExactOutliers(points, D, M, K)
+
+
