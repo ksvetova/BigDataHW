8a1524bba747a3bdb46082d1d767de99ce069c1c
ksvetova <58701924+ksvetova@users.noreply.github.com>
2024-04-07 17:36:46 +0200

    changed conditions

--- HW1/approximateAlgorithm.py
+++ HW1/approximateAlgorithm.py
@@ -1,12 +1,13 @@
 from pyspark import SparkContext, SparkConf
 import csv
+import math
 
 def MRApproxOutliers(points_rdd, D, M, K):
     #count points in each cell
     def map_to_cell(point):
         x, y = point
-        cell_i = int(x / (D / (2 * (2 ** 0.5))))
-        cell_j = int(y / (D / (2 * (2 ** 0.5))))
+        cell_i = math.floor(x / (D / (2 * (2 ** 0.5))))
+        cell_j = math.floor(y / (D / (2 * (2 ** 0.5))))
         return ((cell_i, cell_j), 1)
 
     cells_rdd = points_rdd.map(map_to_cell).reduceByKey(lambda a, b: a + b).cache()
@@ -25,14 +26,14 @@ def MRApproxOutliers(points_rdd, D, M, K):
     cell_info_rdd = cells_rdd.map(map_to_N3_N7).cache()
 
     #count sure and uncertain outliers
-    sure_outliers_count = cell_info_rdd.filter(lambda x: x[1][1] > M).count()
+    sure_outliers_count = cell_info_rdd.filter(lambda x: x[1][2] <= M).count()
     uncertain_points_count = cell_info_rdd.filter(lambda x: x[1][1] <= M and x[1][2] > M).count()
 
     print("Sure outliers ({}-outliers): {}".format(M, sure_outliers_count))
     print("Uncertain points: {}".format(uncertain_points_count))
 
     #get the first K non-empty cells sorted by size
-    top_cells = cell_info_rdd.sortBy(lambda x: x[1][0], ascending=False).take(K)
+    top_cells = cell_info_rdd.sortBy(lambda x: x[1][0], ascending=True).take(K)
 
     print("Top {} non-empty cells:".format(K))
     for cell, info in top_cells:
@@ -44,16 +45,16 @@ if __name__ == "__main__":
     sc = SparkContext(conf=conf)
     
     points = []
-    # with open('test.txt', "r") as file:
-    #     for line in file:
-    #         coord = line.strip().split(',')
-    #         points.append((float(coord[0]), float(coord[1])))
+    with open('test.txt', "r") as file:
+        for line in file:
+            coord = line.strip().split(',')
+            points.append((float(coord[0]), float(coord[1])))
 
-    with open('uber-10k.csv', "r") as file:
-        csv_reader = csv.reader(file)
-        for row in csv_reader:
-            coord = tuple(map(float, row))
-            points.append(coord)
+    # with open('uber-10k.csv', "r") as file:
+    #     csv_reader = csv.reader(file)
+    #     for row in csv_reader:
+    #         coord = tuple(map(float, row))
+    #         points.append(coord)
 
 
     points_rdd = sc.parallelize(points)
