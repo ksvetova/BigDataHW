eef45fca8de71d8cee712ce0ecc0ee375d6f69d2
Luca Pellegrini <luca.pellegrini99@hotmail.it>
2024-04-10 12:53:02 +0200

    faster ExactOutliers

--- HW1/ExactOutliers.py
+++ HW1/ExactOutliers.py
@@ -1,15 +1,39 @@
 import csv
 import math
 
-# function to compute distance between 2 points
-def distance(a, b):
-    # a = [x1, y1]
-    # b = [x2, y2]
-    d = math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
-    return d
-
 # function to implement the exact algorithm
 def ExactOutliers(points, D, M, K):
+
+    points_count = []
+
+    for i in range(0, len(points)):
+        points_count.append(1)
+
+    for i in range(0, len(points)):
+        for j in range(0+i+1, len(points)):
+            if math.dist(points[i], points[j]) <= D:
+                points_count[i] = points_count[i] + 1
+                points_count[j] = points_count[j] + 1
+
+    outliers = []
+
+    for i in range(0, len(points)):
+        if points_count[i] <= M:
+            outliers.append((points[i], points_count[i]))
+
+    print('Number of ouliers =', len(outliers))
+
+    ouliers_sort = sorted(outliers, key=lambda a: a[1])
+
+    k = 0
+
+    if K > len(outliers):
+        K = len(outliers)
+    
+    for i in range(k, K):
+        print("Point:", "({},{})".format(ouliers_sort[i][0][0], ouliers_sort[i][0][1]))
+
+def ExactOutliersSlow(points, D, M, K):
 
     outliers = []
 
